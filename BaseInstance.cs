7cce459eb6f387c626f557c641d1f9828d49ccb8
Mohammad <m.ferdosi94@gmail.com>
2021-08-29 20:57:34 +0430

    Update BaseInstance.cs

--- BaseInstance.cs
+++ BaseInstance.cs
@@ -4,6 +4,7 @@ namespace Merge
 {
     public abstract class BaseInstance
     {
+        public Guid InstanceGuid { get; set; }
         public long ElementId { get; set; }
         public List<Attribute> Attributes { get; set; }
     }
